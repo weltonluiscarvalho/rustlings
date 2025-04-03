4e72b446089b4d9855019285b5c1f49028fcb4a8
Welton Carvalho <welton.carvalho.dev@gmail.com>
2025-04-02 21:32:32 -0300

    resolved intro, variables and functions

--- exercises/02_functions/functions1.rs
+++ exercises/02_functions/functions1.rs
@@ -1,4 +1,7 @@
 // TODO: Add some function with the name `call_me` without arguments or a return value.
+fn call_me() {
+    println!("yes, i will call!");
+}
 
 fn main() {
     call_me(); // Don't change this line
